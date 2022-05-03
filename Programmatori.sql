2ea24c00234512668ccf24798b1c0e790dd36f19
AlenaBunina <alena.bunina03@gmail.com>
2022-05-03 12:18:36 +0200

    Esercizio2

--- Programmatori.sql
+++ Programmatori.sql
@@ -7,3 +7,10 @@ from autore join programmatore on autore.codice = programmatore.codice /* che so
     join programma on programma.id = autore.id
 where programma.linguaggio = "Java" and programma.anno > 2000 /*scritto in Java dopo 2000
 
+/* 2. */
+
+select programmatore.nome, programmatore.categoria    /* calcolare il nome e la caterogia dei programmatori */
+from autore join programmatore on autore.codice = programmatore.codice /* che sono stati autori di almeno un programma */
+    join programma on programma.id = autore.id  /* quindi il ID programmatore uguale ID Autore, ID programma uguale */
+where programma.linguaggio != "Python" /* in un linguaggio diverso da Python */
+order by programmatore.nome /* ordinando per il nome dei programmatori */
