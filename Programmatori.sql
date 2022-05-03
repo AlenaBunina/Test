19f174c3d6605034dcba67ca160ea809db8fa8f4
AlenaBunina <alena.bunina03@gmail.com>
2022-05-03 12:17:50 +0200

    Esercizio1

--- Programmatori.sql
+++ Programmatori.sql
@@ -1,1 +1,9 @@
 /* Esercizio 1 */
+
+/* 1. */
+
+select programmatore.codice /*calcolare il codice dei programmatori */
+from autore join programmatore on autore.codice = programmatore.codice /* che sono stati autori di almeno un programma */
+    join programma on programma.id = autore.id
+where programma.linguaggio = "Java" and programma.anno > 2000 /*scritto in Java dopo 2000
+
